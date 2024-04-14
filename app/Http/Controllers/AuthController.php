849fe890e8a189bea5a36d1c7e5ef714487d61d3
Carlos Loitti <carlosloitir@gmail.com>
2024-04-14 17:08:31 -0500

    first commit

---
+++ app/Http/Controllers/AuthController.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\User;
+use Illuminate\Http\Request;
+use App\Http\Requests\LoginRequest;
+use Illuminate\Support\Facades\Auth;
+use App\Http\Requests\RegistroRequest;
+
+class AuthController extends Controller
+{
+    
+    public function register(RegistroRequest $request) {
+        $data = $request->validated();
+
+        $user = User::create([
+            'name' => $data['name'],
+            'email' => $data['email'],
+            'password' => bcrypt($data['password'])
+        ]);
+
+        return [
+            'token' => $user->createToken('token')->plainTextToken,
+            'user' => $user
+        ];
+    }
+
+    public function login(LoginRequest $request) {
+        $data = $request->validated();
+
+
+        if(!Auth::attempt($data)) {
+            return response([
+                'errors' => ['El email o contraseña son incorrectos']
+            ], 422);
+        }
+
+        $user = Auth::user();
+        return [
+            'token' => $user->createToken('token')->plainTextToken,
+            'user' => $user
+        ];
+
+    }
+
+    public function logout(Request $request) {
+        $user = $request->user();
+        $user->currentAccessToken()->delete();
+
+        return [
+            'user' => null
+        ];
+    }
+}
